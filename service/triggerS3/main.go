94f061091f56129caf2c4b91b4b69cc9a49e4443
Bartlomiej Malecki <bmalecki995@gmail.com>
2020-05-19 01:47:07 +0200

    working queue

--- service/triggerS3/main.go
+++ service/triggerS3/main.go
@@ -34,20 +34,20 @@ func pushRecord(qURL string) (*sqs.SendMessageOutput, error) {
 
 	result, err := svc.SendMessage(&sqs.SendMessageInput{
 		DelaySeconds: aws.Int64(10),
-		// MessageAttributes: map[string]*sqs.MessageAttributeValue{
-		// 	"Title": {
-		// 		DataType:    aws.String("String"),
-		// 		StringValue: aws.String("The Whistler"),
-		// 	},
-		// 	"Author": {
-		// 		DataType:    aws.String("String"),
-		// 		StringValue: aws.String("John Grisham"),
-		// 	},
-		// 	"WeeksOn": {
-		// 		DataType:    aws.String("Number"),
-		// 		StringValue: aws.String("6"),
-		// 	},
-		// },
+		MessageAttributes: map[string]*sqs.MessageAttributeValue{
+			"Title": {
+				DataType:    aws.String("String"),
+				StringValue: aws.String("The Whistler"),
+			},
+			"Author": {
+				DataType:    aws.String("String"),
+				StringValue: aws.String("John Grisham"),
+			},
+			"WeeksOn": {
+				DataType:    aws.String("Number"),
+				StringValue: aws.String("6"),
+			},
+		},
 		MessageBody: aws.String("Information about current NY Times fiction bestseller for week of 12/11/2016."),
 		QueueUrl:    &qURL,
 	})

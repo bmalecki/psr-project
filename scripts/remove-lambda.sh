0d2f7339b2942bef26e56fd92d263a39d0158b48
Bartlomiej Malecki <bmalecki995@gmail.com>
2020-05-19 17:19:17 +0200

    add analyze consumer

--- scripts/remove-lambda.sh
+++ scripts/remove-lambda.sh
@@ -4,6 +4,6 @@ export AWS_SHARED_CREDENTIALS_FILE=$PWD/terraform/secrets/aws.credentials
 aws cloudformation describe-stack-resources --stack-name uploadservice-dev \
     --query 'StackResources[?ResourceType == `AWS::S3::Bucket`].[PhysicalResourceId]' --output text \
     | xargs -d ' ' \
-    | xargs -I% aws s3 rm s3://%/ --recursive
+    | xargs -I% aws s3 rm s3://%/ --recursive > /dev/null
 
 (cd service && npx serverless remove)
--- service/analyze/main.go
+++ service/analyze/main.go
@@ -2,11 +2,7 @@ package main
 
 import (
 	"context"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/aws/aws-sdk-go/service/s3/s3manager"
+	"fmt"
 
 	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
@@ -15,33 +11,24 @@ import (
 type Reqeust events.APIGatewayProxyRequest
 type Response events.APIGatewayProxyResponse
 
-var svc *s3.S3
-var uploader *s3manager.Uploader
+// var svc *s3.S3
+// var uploader *s3manager.Uploader
 
-func init() {
-	sess, err := session.NewSession(&aws.Config{
-		Region: aws.String("us-east-1")},
-	)
-	if err != nil {
-		panic(err)
-	}
+// func init() {
+// 	sess, err := session.NewSession(&aws.Config{
+// 		Region: aws.String("us-east-1")},
+// 	)
+// 	if err != nil {
+// 		panic(err)
+// 	}
 
-	svc = s3.New(sess)
-	uploader = s3manager.NewUploader(sess)
-}
+// 	svc = s3.New(sess)
+// 	uploader = s3manager.NewUploader(sess)
+// }
 
-func createResponse(statusCode int, msg string) Response {
-	return Response{
-		StatusCode: 500,
-		Body:       msg,
-		Headers: map[string]string{
-			"Content-Type": "plain/text",
-		},
-	}
-}
-
-func Handler(ctx context.Context, req Reqeust) (Response, error) {
-	return createResponse(200, "OK"), nil
+func Handler(ctx context.Context, req Reqeust) error {
+	fmt.Println("Recieved")
+	return nil
 }
 
 func main() {
